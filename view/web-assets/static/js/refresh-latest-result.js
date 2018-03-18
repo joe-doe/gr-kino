54ef6989067fe6902b1ddbc76d84b60af2565430
Joe Doe <joe.doe@mail.com>
2018-03-18 21:14:05 +0200

    bug on refresh

--- view/web-assets/static/js/refresh-latest-result.js
+++ view/web-assets/static/js/refresh-latest-result.js
@@ -1,7 +1,6 @@
 var updater = function () {
-                           ('#latest-result').load('latest-result').fadeIn("slow");
+                           $('#latest-result').load('latest-result').fadeIn("slow");
                            $.getJSON( "/last-draw", function( d ) {
-                              console.log(d);
                               var draw_no = d.drawNo;
                               var draw_time = d.drawTime;
                               var draw_results = d.results;
@@ -15,7 +14,7 @@ var updater = function () {
                               $("#"+draw_bonus).removeClass();
                               $("#"+draw_bonus).addClass("bonus");
 
-                           );
-                           ;
+                           });
+                           };
 updater();
 var autoLoad = setInterval( updater, 60000*5); // refresh page every 60*5 seconds
